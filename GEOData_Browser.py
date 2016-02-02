16b073e143d326b7c1823a567e277e5803104641
juancholkovich <judhenaosa@unal.edu.co>
2016-02-02 16:28:07 -0500

    creating finder

--- GEOData_Browser.py
+++ GEOData_Browser.py
@@ -3,6 +3,9 @@ import re
 #path = raw_input("Enter the path of the gds file\n")
 new_file = ""
 
+def finder(x):
+    print x
+
 try:
     infile = open("gds_result.txt")
 
@@ -13,3 +16,7 @@ except:
     print "The file do not exist"
 
 data = re.split("\n(\d+)\. ",new_file)
+
+for n in data:
+    if re.match("\D",n):
+        finder(n)
