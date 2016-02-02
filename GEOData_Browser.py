cff3e8753b620e3e1b1a755e519f19ae0c3060ab
JuanHenao <judhenaosa@unal.edu.co>
2016-02-01 20:03:39 -0500

    reading file

--- GEOData_Browser.py
+++ GEOData_Browser.py
@@ -1,9 +1,21 @@
 import re
 
+#path = raw_input("Enter the path of the gds file\n")
+new_file = []
+
 try:
     infile = open("/home/juan/Descargas/gds_result.txt")
 
     for i in infile.readlines():
-        print i
+        new_file.append(i)
+
 except:
-    print "l cagate"
+    print "The file Do not exist"
+
+range_init = 0
+range_final = 8
+
+while range_final <= len(new_file):
+    print new_file[range_init:range_final]
+    range_init += 8
+    range_final +=8
