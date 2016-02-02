4447a37b1e6ccf11cd4d5483a8169a95e69249f1
JuanHenao <judhenaosa@unal.edu.co>
2016-02-01 21:35:51 -0500

    re-edit

--- GEOData_Browser.py
+++ GEOData_Browser.py
@@ -1,22 +1,15 @@
 import re
 
 #path = raw_input("Enter the path of the gds file\n")
-new_file = []
+new_file = ""
 
 try:
     infile = open("/home/juan/Descargas/gds_result.txt")
 
     for i in infile.readlines():
-        new_file.append(i)
+        new_file += i
 
 except:
-    print "The file Do not exist"
+    print "The file do not exist"
 
-range_init = 0
-range_final = 6
-
-while range_final <= len(new_file):
-    for n in new_file[range_init:range_final]:
-        print n
-    range_init += 6
-    range_final += 6
+print new_file[1]
