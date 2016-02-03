d9743aead25aea27f47043e3760340dcd6e5c99f
juancholkovich <judhenaosa@unal.edu.co>
2016-02-03 10:04:54 -0500

    outputs

--- GEOData_Browser.py
+++ GEOData_Browser.py
@@ -5,6 +5,7 @@ new_file = ""
 dic_chip = {}
 
 def chip_counter(z):
+    print z
     t = re.search("(GPL)\d*",z)
     if dic_chip.has_key(t.group(0)):
         dic_chip[t.group(0)] += 1
@@ -22,13 +23,18 @@ def first_filter(x):
         second_filter(x)
 
 try:
-    infile = open("gds_result.txt")
+    infile = open("gds_result.txt","r")
 
     for i in infile.readlines():
         new_file += i
 
 except:
     print "The file do not exist"
+
+try:
+    outfile = open("Parkinson_Chips.txt","w")
+except:
+    print "File is not created"
 
 data = re.split("\n(\d+)\. ",new_file)
 
@@ -38,3 +44,7 @@ for n in data:
 
 for w in dic_chip.items():
     print  w[0],": ",w[1]
+    outfile.write(w[0],": ",w[1])
+
+infile.close()
+outfile.close()
