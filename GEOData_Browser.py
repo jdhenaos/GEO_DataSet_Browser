2b415e9790c6a485b1bf7ecbf669b4f66b7b97dc
JuanHenao <judhenaosa@unal.edu.co>
2016-02-04 20:29:11 -0500

    init third filter

--- GEOData_Browser.py
+++ GEOData_Browser.py
@@ -4,26 +4,25 @@ import re
 new_file = ""
 dic_chip = {}
 
-def chip_counter(z):
-    outfile2.writelines(z+"\n")
-    t = re.search("(GPL)\d*",z)
-    if dic_chip.has_key(t.group(0)):
-        dic_chip[t.group(0)] += 1
-    else:
-        dic_chip[t.group(0)] = 1
+#def chip_counter(z):
+#    outfile2.writelines(z+"\n")
+#    t = re.search("(GPL)\d*",z)
+#    if dic_chip.has_key(t.group(0)):
+#        dic_chip[t.group(0)] += 1
+#    else:
+#        dic_chip[t.group(0)] = 1
+
+def third_filter(w):
+    
 
 def second_filter(y):
     init_pos = y.find("Organism:")
     final_pos = y.find(":",init_pos+9)
     if y[init_pos:final_pos].find(";") == -1:
-        chip_counter(y)
+        third_filter(y)
 
 def first_filter(x):
-    if x.find("multiple sclerosis") != -1:
-        second_filter(x)
-    elif x.find("Multiple sclerosis") != -1:
-        second_filter(x)
-    elif x.find("Multiple Sclerosis") != -1:
+    if x.find("Parkinson") != -1:
         second_filter(x)
 
 try:
