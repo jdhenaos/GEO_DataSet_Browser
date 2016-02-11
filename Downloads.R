f62f72acefa036a93e361aee8931829417a30116
JuanHenao <judhenaosa@unal.edu.co>
2016-02-10 22:38:36 -0500

    real data

--- Downloads.R
+++ Downloads.R
@@ -1,34 +1,75 @@
 library("GEOquery")
 
-# Primera descarga
-
-getGEO(GEO = "GSE6613",GSEMatrix = TRUE,destdir = ".")
-
-GSE6613<-read.csv(gzfile("GSE6613_series_matrix.txt.gz"),
+# Parkinson (GPL96)
+# --1--
+getGEO(GEO = "GSE35642",GSEMatrix = TRUE,destdir = ".")
+GSE35642<-read.csv(gzfile("GSE35642_series_matrix.txt.gz"),
                    comment.char = "!", 
                    sep = "\t",
                    stringsAsFactors = FALSE)
+# --2--
+getGEO(GEO = "GSE20186",GSEMatrix = TRUE,destdir = ".")
+GSE20186<-read.csv(gzfile("GSE20186-GPL96_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --3--
+getGEO(GEO = "GSE20314",GSEMatrix = TRUE,destdir = ".")
+GSE20314<-read.csv(gzfile("GSE20314_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --dimensiones--
+dim(GSE35642)
+dim(GSE20186)
+dim(GSE20314)
 
-dim(GSE6613)
+# Alzheimer (GPL570)
+# --1--
+getGEO(GEO = "GSE66333",GSEMatrix = TRUE,destdir = ".")
+GSE66333<-read.csv(gzfile("GSE66333_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --2--
+getGEO(GEO = "GSE48350",GSEMatrix = TRUE,destdir = ".")
+GSE48350<-read.csv(gzfile("GSE48350_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --3--
 
-# Segunda descarga
+getGEO(GEO = "GSE53890",GSEMatrix = TRUE,destdir = ".")
+GSE53890<-read.csv(gzfile("GSE53890_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --dimensiones--
 
-getGEO(GEO = "GSE8397",GSEMatrix = TRUE,destdir = ".")
+dim(GSE66333)
+dim(GSE48350)
+dim(GSE53890)
 
-GSE8397<-read.csv(gzfile("GSE8397-GPL96_series_matrix.txt.gz"),
-                  comment.char = "!", 
-                  sep = "\t",
-                  stringsAsFactors = FALSE)
-
-dim(GSE8397)
-
-# Tercera descarga
-
-getGEO(GEO = "GSE20164",GSEMatrix = TRUE,destdir = ".")
-
-GSE20164<-read.csv(gzfile("GSE20164_series_matrix.txt.gz"),
-                  comment.char = "!", 
-                  sep = "\t",
-                  stringsAsFactors = FALSE)
-
-dim(GSE20164)
+# Esclerosis Multiple (GPL570)
+# --1--
+getGEO(GEO = "GSE68527",GSEMatrix = TRUE,destdir = ".")
+GSE68527<-read.csv(gzfile("GSE68527_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --2--
+getGEO(GEO = "GSE59085",GSEMatrix = TRUE,destdir = ".")
+GSE59085<-read.csv(gzfile("GSE59085_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --3--
+getGEO(GEO = "GSE37783",GSEMatrix = TRUE,destdir = ".")
+GSE37783<-read.csv(gzfile("GSE37783_series_matrix.txt.gz"),
+                   comment.char = "!", 
+                   sep = "\t",
+                   stringsAsFactors = FALSE)
+# --dimensiones--
+dim(GSE68527)
+dim(GSE59085)
+dim(GSE37783)
